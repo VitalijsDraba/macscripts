72a57d90527350256e44df0b58151bafcf57f5af
Boatik <boatik@gmail.com>
2018-03-25 12:54:35 +0300

    py script added

--- alfred.workflows/Convert home videos.alfredworkflow.py
+++ alfred.workflows/Convert home videos.alfredworkflow.py
@@ -10,18 +10,16 @@ def main(args):
         notify('Conversion process',
                'File %d of %d' % (i,totalFiles),
                'Processing [%s]' % ntpath.basename(file))
-               if (os.path.isfile(file)):
-                   os.system("""
-                       /usr/local/bin/ffmpeg -y -i "{}" -c:v libx264 -crf 23 -c:a aac -b:a 192k "{}"
-                       """ \
-                             .format(file,
-                                     os.path.splitext(file)[0]+'.264.mp4'))
+        if (os.path.isfile(file)):
+           os.system("""
+               /usr/local/bin/ffmpeg -y -i "{}" -c:v libx264 -crf 23 -c:a aac -b:a 192k "{}"
+               """ \
+               .format(file, os.path.splitext(file)[0]+'.264.mp4'))
+        i+=1
 
-    i+=1
-
-notify('Conversion process finished',
-       'Processed %d files' % totalFiles,
-       'Time spent %s' % time.strftime("%H:%M:%S",time.gmtime(time.time() - start_time)))
+    notify('Conversion process finished',
+           'Processed %d files' % totalFiles,
+           'Time spent %s' % time.strftime("%H:%M:%S",time.gmtime(time.time() - start_time)))
 
 def notify(title, subtitle, message):
     os.system("""
